0b61794e2a67efbfd84ea98b38eb9a17a3ddf524
Tóth Fedor László <toth.fedor@gmail.com>
2023-02-04 17:22:53 +0100

    Oldal

---
+++ app.js
@@ -0,0 +1,34 @@
+    let magic8Ball = {};
+    listOfAnswers = ["Nem!", "Igen!", "Nem hiszem...", "Persze!", "Kétségtelenül!", "Csak álmaidban..."];
+    
+    $("#answer").text( "A válasz...");
+    $("#questionid").text("");
+    $("#questionButton").click(onClick);
+
+    function onClick(){
+        $("#answer").hide();
+        newQuestion();
+    }
+
+    function newQuestion(){
+        let question = prompt("ASK A YES/NO QUESTION!");
+        $("#questionid").text(question);
+        askQuestion(question);
+    }
+  
+    function askQuestion(question) {
+      $("#ball8").effect("shake");
+      $("#answer").fadeIn(4000);
+  
+      let randomNumber = Math.random();
+      let randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+      let randomIndex = Math.floor(randomNumberForListOfAnswers);
+  
+      let answer = this.listOfAnswers[randomIndex];
+      $("#answer").text(answer);
+  
+      console.log(question);
+      console.log(answer);
+    };
+  
+
